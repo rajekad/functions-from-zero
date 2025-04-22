b76bd214747cd3c672c2116dd38431d5c92b96f9
rajekad <raje_kad@yahoo.com>
2025-04-22 03:02:03 +0000

    adding code to repo

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("hellow world")
+# print("hellow world")
+def add(x, y):
+    return x + y
+
+
+print(add(2, 2))
--- myStatements.py
+++ myStatements.py
@@ -1,13 +1,23 @@
-my_list = ["apple","cherry","lemon"]
-my_dict = {"drink": "coffee","milk": "whole"}
+my_list = ["apple", "cherry", "lemon"]
+my_dict = {"drink": "coffee", "milk": "whole"}
 
 for item in my_list:
     print(f"My favorite thing to eat is: {item}")
 
-for _,value in my_dict.items():
+for _, value in my_dict.items():
     print(f"My favorite thing is: {value}")
 
 # potential problem
 # my_dict = my_dict
 # mylist2 =
 # use git add * and git status to check status
+# then commit by typing:  git commit -m "adding files to repo" in the terminal
+# the do : git push in the terminal
+
+# create Makefile file using touch Makefile in terminal
+# create requirement file using : touch requirements.txt in terminal
+# create some kind of test file in terminal: touch test_hello.py
+# type make install in termilal so that it will let us lint and test code
+# type make lint in termilan to ched for all errors
+# then type make all, git status, git add *, commit -m 'adding code to repo 
+# and git push one by one in terminal
