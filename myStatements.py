bdcb7e3dd71e63ce973c4b1e91eb3ba078c153b7
rajekad <raje_kad@yahoo.com>
2025-04-22 02:25:35 +0000

    adding files to the repo

---
+++ myStatements.py
@@ -0,0 +1,13 @@
+my_list = ["apple","cherry","lemon"]
+my_dict = {"drink": "coffee","milk": "whole"}
+
+for item in my_list:
+    print(f"My favorite thing to eat is: {item}")
+
+for _,value in my_dict.items():
+    print(f"My favorite thing is: {value}")
+
+# potential problem
+# my_dict = my_dict
+# mylist2 =
+# use git add * and git status to check status
